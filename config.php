87637dfed18953b0797703c07162c8e0929b177c
rubeeengm <rubeeengm@outlook.com>
2019-11-13 23:50:53 -0600

    carga de assets y archivo de configuración para base de datos

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('__CONFIG__', [
+    'db' => [
+        'host' => 'mysql:host=localhost; dbname=VENTALIBROS; chartset=utf8'
+        , 'user' => 'root'
+        , 'password' => ''
+    ]
+]);
