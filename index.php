e3736e5edac1e6b6228c5f36336b243ae7a05b35
rubeeengm <rubeeengm@example.com>
2019-11-17 21:34:00 -0600

    carga de imagenes para carrusel, carga de templates para footer, header y menu

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types = 1);
+
+require_once 'vista/template/header.php';
+require_once 'vista/principal/principal.php';
+require_once 'vista/template/footer.php';
---
+++ vista/template/footer.php
@@ -0,0 +1,9 @@
+		<script src="assets/js/jquery-3.4.1.min.js"></script>
+		<script src="assets/js/popper.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+
+		<script type="text/javascript">
+			$('.carousel').carousel();
+		</script>
+	</body>
+</html>
